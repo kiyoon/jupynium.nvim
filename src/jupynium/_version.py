e57c0140bbdd837cd0c0b73a388ef799398a616e
Kiyoon Kim <yoonkr33@gmail.com>
2023-01-13 04:50:16 +0000

    vendored mode versioneer with legacy build meta

--- src/jupynium/_version.py
+++ src/jupynium/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = ""
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "jupynium-"
     cfg.versionfile_source = "src/jupynium/_version.py"
     cfg.verbose = False
