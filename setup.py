9f6efb4e76604f1e11d969c64c9c1ddb2933e62d
Kiyoon Kim <yoonkr33@gmail.com>
2023-01-16 19:36:04 +0000

    setuptools-scm

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-import sys
-
 from setuptools import setup
 
-import versioneer
-
-sys.path.insert(0, ".")
-
 if __name__ == "__main__":
-    setup(
-        version=versioneer.get_version(),
-        cmdclass=versioneer.get_cmdclass(),
-    )
+    setup()
--- src/jupynium/__init__.py
+++ src/jupynium/__init__.py
@@ -1,3 +1,17 @@
-from . import _version
+try:
+    from importlib.metadata import PackageNotFoundError, version
 
-__version__ = _version.get_versions()["version"]
+    try:
+        __version__ = version("jupynium")
+    except PackageNotFoundError:
+        # package is not installed
+        __version__ = "unknown"
+except ImportError:
+    # For versions of python before 3.8
+    from pkg_resources import DistributionNotFound, get_distribution
+
+    try:
+        __version__ = get_distribution("jupynium").version
+    except DistributionNotFound:
+        # package is not installed
+        __version__ = "unknown"
