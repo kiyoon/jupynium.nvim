e730e34fce0015f6227dfa77cf48c525cca366a2
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-06-07 13:51:40 +0900

    refactor!: remove deprecated markdown syntax (#121)

--- after/queries/python/injections.scm
+++ after/queries/python/injections.scm
@@ -1,13 +1,5 @@
 ; extends
 
-; match a string that starts with """%% and ends with %%"""
-; or starts with '''%% and ends with %%'''
-; and highlight it as markdown
-(expression_statement
- ((string) @markdown @markdown_inline)
- (#match? @markdown_inline "^[\"']{3}[%]{2}.*[%]{2}[\"']{3}$")
-)
-
 ; it can be # %% [markdown] or # %% [md]
 ((
   (comment) @_mdcomment
