287e7f30a574d76dd07d62aacb4b4a89b3a79687
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-06-11 12:06:50 +0900

    build: faster with uv

---
+++ scripts/build_with_uv.sh
@@ -0,0 +1,21 @@
+
+if [[ $# -eq 0 ]]; then
+    echo "Usage: $0 <path-to-venv>"
+    exit 1
+fi
+
+path_to_venv="$1"
+
+if [[ -f "$path_to_venv" ]]; then
+    echo "Error: $path_to_venv is a file"
+    exit 1
+fi
+
+if [[ ! -d "$path_to_venv" ]]; then
+    uv venv "$path_to_venv"
+fi
+
+source "$path_to_venv/bin/activate"
+uv pip install .
+mkdir -p "$HOME/.local/bin"
+ln -sf "$path_to_venv/bin/python-import" "$HOME/.local/bin"
