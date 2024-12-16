b0b71577c6e4a613b6a4180068dd0b2de0659e52
Kiyoon Kim <kiyoon@users.noreply.github.com>
2024-12-16 13:16:04 +0900

    fix(treesitter): markdown injection

--- after/queries/python/injections.scm
+++ after/queries/python/injections.scm
@@ -4,11 +4,27 @@
 ((
   (comment) @_mdcomment
   . (expression_statement 
-      (string (string_content) @markdown @markdown_inline)))
-  (#lua-match? @_mdcomment "^# %%%% %[markdown%]"))
+      (string (string_content) @injection.content)))
+  (#lua-match? @_mdcomment "^# %%%% %[markdown%]")
+  (#set! injection.language "markdown"))
 
 ((
   (comment) @_mdcomment
   . (expression_statement 
-      (string (string_content) @markdown @markdown_inline)))
-  (#lua-match? @_mdcomment "^# %%%% %[md%]"))
+      (string (string_content) @injection.content)))
+  (#lua-match? @_mdcomment "^# %%%% %[markdown%]")
+  (#set! injection.language "markdown_inline"))
+
+((
+  (comment) @_mdcomment
+  . (expression_statement 
+      (string (string_content) @injection.content)))
+  (#lua-match? @_mdcomment "^# %%%% %[md%]")
+  (#set! injection.language "markdown"))
+
+((
+  (comment) @_mdcomment
+  . (expression_statement 
+      (string (string_content) @injection.content)))
+  (#lua-match? @_mdcomment "^# %%%% %[md%]")
+  (#set! injection.language "markdown_inline"))
