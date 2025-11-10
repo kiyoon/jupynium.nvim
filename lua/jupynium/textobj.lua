53200cbd2c277251c47ef4ffc1ead5051a3c1a85
Kiyoon Kim <kiyoon@users.noreply.github.com>
2025-11-10 14:04:44 +0900

    feat: repeatable move compatibility for nvim-treesitter-textobjects main branch

--- lua/jupynium/textobj.lua
+++ lua/jupynium/textobj.lua
@@ -86,10 +86,70 @@ M.goto_next_cell_separator = function()
   vim.api.nvim_win_set_cursor(0, { row, 0 })
 end
 
-local status, repeat_move = pcall(require, "nvim-treesitter.textobjects.repeatable_move")
+local status, repeat_move = pcall(require, "nvim-treesitter-textobjects.repeatable_move")
 if status then
+  -- move_fn's first argument must be a table of options, and it should include a `forward` boolean
+  -- indicating whether to move forward (true) or backward (false)
+  local set_last_move = function(move_fn, opts, ...)
+    if type(move_fn) ~= "function" then
+      vim.notify(
+        "nvim-treesitter-textobjects: move_fn has to be a function but got " .. vim.inspect(move_fn),
+        vim.log.levels.ERROR
+      )
+      return false
+    end
+
+    if type(opts) ~= "table" then
+      vim.notify(
+        "nvim-treesitter-textobjects: opts has to be a table but got " .. vim.inspect(opts),
+        vim.log.levels.ERROR
+      )
+      return false
+    elseif opts.forward == nil then
+      vim.notify(
+        "nvim-treesitter-textobjects: opts has to include a `forward` boolean but got " .. vim.inspect(opts),
+        vim.log.levels.ERROR
+      )
+      return false
+    end
+
+    repeat_move.last_move = { func = move_fn, opts = vim.deepcopy(opts), additional_args = { ... } }
+    return true
+  end
+
+  -- Get a movement function pair (forward, backward) and turn them into two repeatable movement functions
+  -- They don't need to have the first argument as a table of options
+  local function make_repeatable_move_pair(forward_move_fn, backward_move_fn)
+    local general_repeatable_move_fn = function(opts, ...)
+      if opts.forward then
+        forward_move_fn(...)
+      else
+        backward_move_fn(...)
+      end
+    end
+
+    local repeatable_forward_move_fn = function(...)
+      set_last_move(general_repeatable_move_fn, { forward = true }, ...)
+      forward_move_fn(...)
+    end
+
+    local repeatable_backward_move_fn = function(...)
+      set_last_move(general_repeatable_move_fn, { forward = false }, ...)
+      backward_move_fn(...)
+    end
+
+    return repeatable_forward_move_fn, repeatable_backward_move_fn
+  end
+
   M.goto_next_cell_separator, M.goto_previous_cell_separator =
-    repeat_move.make_repeatable_move_pair(M.goto_next_cell_separator, M.goto_previous_cell_separator)
+    make_repeatable_move_pair(M.goto_next_cell_separator, M.goto_previous_cell_separator)
+else
+  -- master branch compatibility
+  status, repeat_move = pcall(require, "nvim-treesitter.textobjects.repeatable_move")
+  if status then
+    M.goto_next_cell_separator, M.goto_previous_cell_separator =
+      repeat_move.make_repeatable_move_pair(M.goto_next_cell_separator, M.goto_previous_cell_separator)
+  end
 end
 
 M.goto_current_cell_separator = function()
